e56edd078098ca5d9ba5d88b4e7aff3ed5fb2d3d
Aote Xin <axin@ksu.edu>
2024-02-25 16:05:26 -0800

    flask#2

--- hello.py
+++ hello.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# Route for handling the login page logic (user: admin, pw: admin)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    error = None 
+    
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('welcome')) 
+        
+    return render_template('login.html', error=error)
+
+# welcome page after login successfully
+@app.route("/welcome") 
+def welcome():
+    error = None 
+    return render_template('welcome.html', error=error)
